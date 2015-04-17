13624fd6d3b3ad4fd3546aec8c647e0962ef3a80
Arthur <arthur.peka@outlook.com>
2015-04-17 19:43:20 +0300

    adding test for object adding functionality

--- PowerCertmgr.Tests/BaseTests.cs
+++ PowerCertmgr.Tests/BaseTests.cs
@@ -32,6 +32,34 @@ namespace PowerCertmgr.Tests
                 Assert.True(sw.ToString().Contains("Usage:"));
             }
         }
+
+        [Test]
+        public void CallingAddWithoutMandatoryParamsPrintsError()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                PowerCertMgr.Main(new string[]{ "-add", "-c" });
+                Assert.True(sw.ToString().Contains("Invalid arguments: store name wasn't specified"));
+            }
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                PowerCertMgr.Main(new string[]{ "-add", "-crl" });
+                Assert.True(sw.ToString().Contains("Invalid arguments: store name wasn't specified"));
+            }
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                PowerCertMgr.Main(new string[]{ "-add", "-crap" });
+                Assert.True(sw.ToString().Contains("Usage:"));
+            }
+        }
     }
 }
 
